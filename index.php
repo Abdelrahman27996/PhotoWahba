1604a4380787485cc6db1155d7100a9fbec0c9af
Sarah Sameh Wahba <117436397+SarahSameh19@users.noreply.github.com>
2023-12-18 13:14:22 +0200

    adding folders

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include_once 'config/init.php';?>
-<?php
-$product = new Product();
-$template = new Template('templates/frontpage.php');
-
-$template->title ='Photo Wahba';
-
-echo $template;
-?>
